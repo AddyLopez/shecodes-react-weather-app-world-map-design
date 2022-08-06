a7f556ef625f33b144ce238fdc6ae231c14d5be3
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2022-08-06 16:50:14 -0600

    Adds links to GitHub repo & my coding portfolio

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ export default function App() {
   return (
     <div className="App">
       <h1>React Weather Application (Version: Google Design)</h1>
+      <footer>
+        <a
+          href="https://github.com/AddyLopez/shecodes-react-weather-app-google-dashboard-design"
+          title="To this project's GitHub repository"
+          target="_blank"
+        >
+          Open-source code
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://ubiquitous-froyo-69b6d7.netlify.app/index.html"
+          title="To Addy's personal website"
+          target="_blank"
+        >
+          Addy López
+        </a>
+        .
+      </footer>
     </div>
   );
 }
