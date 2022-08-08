525a156e2d419dd4fde879b8478780a509dde582
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2022-08-08 16:56:43 -0600

    Programs units buttons with temporary placeholders

--- src/ConversionButtons.js
+++ src/ConversionButtons.js
@@ -1,14 +1,29 @@
 import React from "react";
 import "./styles/ConversionButtons.css";
 
-export default function ConversionButtons(props) {
+export default function ConversionButtons() {
+  function showFahrenheit() {
+    alert("Return later for this feature!");
+  }
+
+  function showCelsius() {
+    alert("Return later for this feature!");
+  }
+
   return (
     <div className="ConversionButtons">
-      <button type="button" className="btn btn-success">
+      <button
+        type="button"
+        className="btn btn-success"
+        onClick={showFahrenheit}
+      >
         °F
       </button>
-
-      <button type="button" className="btn btn-success ms-2">
+      <button
+        type="button"
+        className="btn btn-success ms-2"
+        onClick={showCelsius}
+      >
         °C
       </button>
     </div>
