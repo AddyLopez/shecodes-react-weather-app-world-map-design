a7d9a4ea962753bf16c1b6427b178e85fd82e0f3
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2022-08-15 00:14:02 -0600

    Adds map container, base layers, & layers control
    Reformatting is underway to integrate the weather app with React Leaflet. App.js now includes the map and nests the weather application inside of the map.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
 import React from "react";
 import Weather from "./Weather";
+import { MapContainer, TileLayer, LayersControl } from "react-leaflet";
 
 import "./styles/App.css";
+
+const { BaseLayer } = LayersControl;
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Weather defaultCity="Albuquerque" />
-        <footer>
-          <a
-            href="https://github.com/AddyLopez/shecodes-react-weather-app-google-dashboard-design"
-            title="To this project's GitHub repository"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open-source code
-          </a>{" "}
-          by{" "}
-          <a
-            href="https://ubiquitous-froyo-69b6d7.netlify.app/index.html"
-            title="To Addy's personal website"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Addy López
-          </a>
-        </footer>
-      </div>
+      <MapContainer
+        className="Map"
+        center={[35.106766, -106.629181]}
+        zoom={6}
+        scrollWheelZoom={false}
+      >
+        <LayersControl position="topright">
+          <BaseLayer name="OpenStreetMap" checked>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+          </BaseLayer>
+          <BaseLayer name="Stamen Toner">
+            <TileLayer
+              url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png"
+              attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              subdomains="abcd"
+              minNativeZoom={0}
+              maxNativeZoom={20}
+            />
+          </BaseLayer>
+          <BaseLayer name="Esri National Geographic map">
+            <TileLayer
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}.png"
+              attribution="Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC"
+              maxNativeZoom={16}
+            />
+          </BaseLayer>
+          <BaseLayer name="Esri World Imagery">
+            <TileLayer
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+              attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+            />
+          </BaseLayer>
+        </LayersControl>
+        <WeatherApp />
+      </MapContainer>
     </div>
   );
 }
---
+++ src/WeatherApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Weather from "./Weather";
+
+import "./styles/WeatherApp.css";
+
+export default function WeatherApp() {
+  return (
+    <div className="WeatherApp">
+      <Weather defaultCity="Albuquerque" />
+    </div>
+  );
+}
